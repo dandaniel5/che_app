c96130033aee8de3fc639227f9d8ae106e49a115
dandaniel5 <dan.daniels110@gmail.com>
2022-06-07 19:39:57 +0500

    add + chb btn work add red X btn work

--- static/script2.js
+++ static/script2.js
@@ -7,38 +7,59 @@ let value3 = document.getElementById("new-checkbox-text").value;
 let isExpanded = document.getElementById("collapseExample").className;
 let counter = 0;
 
-$('#plus_button').click(function () {
-    // $('#new-checkbox-text').collapse("toggle");
+$('.btn-close').click(function () {
 
-if($("#plus_button").hasClass('btn btn-outline-primary collapsed')){
+var checked = [value1, value2];
+$('input:checkbox').each(function () {
+     if ($(this).parent().hasClass('show')) {
+        if ($(this).is(':checked')) {
+            xxx = "checked";
+        } else {
+            xxx = "unchecked"
+        };
+        checked.push($(this).attr('name'), xxx);
+    };
+      });
+
+$.ajax({
+            url: '/chb',
+            type: 'POST',
+            data: JSON.stringify(checked),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            async: false,
+            success: alert('отправлено')
+        });
+// location.reload();
+    });
+
+$('#plus_button').click(function () {
+    if($("#plus_button").hasClass('btn btn-outline-primary collapsed')){
     if($("#new-checkbox-text").val() != ""){
 
-
 let value3 = document.getElementById("new-checkbox-text").value;
-
 let collapse_let_d = "#collapsedCheckbox"+counter;
 let collapse_let = "collapsedCheckbox"+counter;
 let new_checkbox = "<label class='list-group-item'> <input class='form-check-input me-1' type='checkbox' name='"+ value3 +"'>"+" "+ value3 + "</label>";
 let hidden_new_checkbox = "<div class='collapse multi-collapse' id='"+collapse_let+"'>" + new_checkbox + "</div>";
     $("#before").before(hidden_new_checkbox);
-counter = counter + 1;
+    counter = counter + 1;
 
 
     $(collapse_let_d).collapse("toggle");
- // $('#new-checkbox-text').collapse("toggle");
-
-   //
-
-   // }
 
 var checked = [value1, value2];
 $('input:checkbox').each(function () {
-    if ($(this).is(':checked')) {
-        xxx = "checked";
-    } else {
-        xxx = "unchecked"
-    };
-    checked.push($(this).attr('name'), xxx);
+   // console.log(this);
+
+       if ($(this).is(':checked')  ) {
+           xxx = "checked";
+       } else {
+           xxx = "unchecked"
+       };
+
+       checked.push($(this).attr('name'), xxx);
+
 });
 // let total = checked.push($("#new-checkbox-text").val(), "unchecked");
 
@@ -53,7 +74,13 @@ $('input:checkbox').each(function () {
         });
 
 // if ($(#collapsedCheckbox).collapsing() != true) {
-    location.reload();
+
+   // setTimeout(function(){
+ location.reload();
+
+// }, 200);
+
+
 // }
 }
 }
@@ -62,13 +89,22 @@ $('input:checkbox').each(function () {
 $('input:checkbox').click(function () {
 var checked = [value1, value2];
 $('input:checkbox').each(function () {
-    if ($(this).is(':checked')) {
-        xxx = "checked";
-    } else {
-        xxx = "unchecked"
-    };
-    let total = checked.push($(this).attr('name'), xxx);
+
+
+        if ($(this).is(':checked')) {
+            xxx = "checked";
+        } else {
+            xxx = "unchecked"
+        }
+        ;
+         let parent = ($(this).parent())
+           if (parent.hasClass('show')){
+
+        checked.push($(this).attr('name'), xxx);
+    }
 });
+
+
 
         $.ajax({
             url: '/chb',
@@ -81,4 +117,5 @@ $('input:checkbox').each(function () {
         });
     });
 });
+// });
 
